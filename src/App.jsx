9bbaed1ad07a645c8cb0caf3b386f8679db5c620
AfoDy <afodydigitals@gmail.com>
2023-11-01 14:32:14 -0700

    compile components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,38 @@ import './App.css'
 function App() {
 
   return (
-   
+      <>
+      <NavBar />
+      <div class="container">
+        <div class="MainContent">
+          <div>
+            <Body />
+            <Description />
+            <div class="card">
+              <Card 
+                image = "/Football.svg" 
+                copy="Experience the excitement of the game."
+              />
+              <Card 
+            
+                image = "/running.svg" 
+                copy="Experienced track and field coaches."
+              />
+              <Card 
+                image = "/swimming.svg" 
+                copy="Join our swimming team and swim like a pro! "
+              />
+              <Card 
+                  image = "/Basketball.svg" 
+                  copy="Basketball programs for all ages and skill levels."
+              />
+            </div>
+          </div>
+            <SideBar />
+        </div>
+      </div>
+      <Footer />
+    </>
   )
 }
 
